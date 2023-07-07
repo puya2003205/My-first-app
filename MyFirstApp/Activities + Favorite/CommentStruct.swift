262e7f1ac79f48e095d543c833b9341a2e9135b7
Andrei Stanciu <andrei.stanciu@idea-bank.ro>
2023-07-07 16:40:01 +0300

    1.7.3

--- MyFirstApp/Activities + Favorite/CommentStruct.swift
+++ MyFirstApp/Activities + Favorite/CommentStruct.swift
@@ -4,18 +4,12 @@ struct CommentStruct: Identifiable, Codable, Equatable {
     let id: UUID
     var comment: String
     var date: String
-    var activity: Activity?
+    var activity: Activity
     
-    init(id: UUID = UUID(), comment: String, date: String, activity: Activity?) {
+    init(id: UUID = UUID(), comment: String, date: String, activity: Activity) {
         self.id = id
         self.comment = comment
         self.date = date
         self.activity = activity
-    }
-}
-
-extension CommentStruct {
-    static var newComment: CommentStruct {
-        CommentStruct(comment: "", date: "", activity: nil)
     }
 }
--- MyFirstApp/Profile/AllComments.swift
+++ MyFirstApp/Profile/AllComments.swift
@@ -7,20 +7,31 @@ struct AllComments: View {
     var body: some View {
         VStack(alignment: .trailing) {
             List {
-                ForEach(commentsStore.allComments.reversed(), id: \.id) { comment in
-                    HStack {
-                        Text(comment.date)
-                        Spacer()
-                        Text(comment.activity?.title ?? "")
-                        Spacer()
-                        Text(comment.activity?.role?.rawValue ?? "")
-                        Spacer()
-                        Text(comment.comment)
-                            .padding(.horizontal, 10)
-                            .padding(.vertical, 5)
-                            .background(Color.gray)
-                            .cornerRadius(10)
-                            .foregroundColor(.white)
+                ForEach(commentsStore.allComments.reversed(), id: \.id) { commentInAllComments in
+                    
+                        NavigationLink(destination: ActivityDetailsView(activity: commentInAllComments.activity, commentsStore: commentsStore)) {
+                            HStack {
+                                Text(commentInAllComments.date)
+                                Spacer()
+                                Text(commentInAllComments.activity.title)
+                                Spacer()
+                                Text(commentInAllComments.activity.role?.rawValue ?? "")
+                                Spacer()
+                                Text(commentInAllComments.comment)
+                                    .padding(.horizontal, 10)
+                                    .padding(.vertical, 5)
+                                    .background(Color.gray)
+                                    .cornerRadius(10)
+                                    .foregroundColor(.white)
+                        }
+                        .simultaneousGesture(TapGesture().onEnded{
+                            commentsStore.nameForDetailsFile = commentInAllComments.activity.id.uuidString
+                            Task {
+                                do {
+                                    try await commentsStore.loadComments()
+                                }
+                            }
+                        })
                     }
                 }
             }
